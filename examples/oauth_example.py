27e1415e4b4a560692027b1bd87473e69b57a42d
Jason Rubenstein <jasondrubenstein@gmail.com>
2012-09-08 07:25:39 -0700

    update docstring to reflect new code

--- examples/oauth_example.py
+++ examples/oauth_example.py
@@ -1,11 +1,11 @@
 import py7D
 
 ''' 
-    How to get an access token: 
-        auth = Oauth7digital(CONSUMER_KEY, CONSUMER_SECRET)
-        token = auth.request_token()
-        authorized = auth.authorize_request_token(token)
-        access_token = auth.request_access_token(token)
+    How to get an access token:
+        import oauth7digital as oa7d
+        token = oa7d.request_token()
+        authorized = oa7d.authorize_request_token(token)
+        access_token = oa7d.request_access_token(token)
 '''
 
 def get_locker(access_token):
