df710ae03511cfb7765a53356007a75efac90676
Jason Rubenstein <jasondrubenstein@gmail.com>
2012-09-08 13:06:54 -0700

    add more info to docstring

--- lib/api_settings.py
+++ lib/api_settings.py
@@ -1,4 +1,7 @@
-''' your local settings '''
+''' your local settings.
+    Note that if you install using setuptools, chnage this
+    module first before running seyup.py install.
+'''
 
 oauthkey = ''
 secret = ''
